490bb018d0969952056c06bc6101e4c31044a06f
Jawnathan <jonathan@jonathanschimpf.com>
2020-11-06 12:47:06 -0500

    intitial setup of our express react app and basic structural wireframe of components and how the application will be organized.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function App() {
+    
+    return (
+      <>
+
+        <h1><strong> HELLO WORLD! </strong></h1>
+
+      </>
+    );
+  };
+
+
+export default App;
