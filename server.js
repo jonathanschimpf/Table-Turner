ad77f286c6e107ca2a3712675b54200ef46935fe
James Lamb <jameslamb13@gmail.com>
2020-11-06 21:02:02 -0500

    started passport setup.

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+const mongoose = require ("mongoose");
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const passportLocal = require("passport-local").Strategy;
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcrypt.js");
+const expressSession = require("express-session");
+const bodyParser = require("body-parser")
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
