152445f4bbde0155b739cdd23ae9d258962cebc9
Jawnathan <jonathan@jonathanschimpf.com>
2020-11-09 14:05:55 -0500

    adding a menu import component with required forms and a categorical dropdown for dish courses

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,10 @@ import React from "react";
 // import LoginComp from "./components/Login/Login";
 import NavbarComp from "./components/Navbar/Navbar"
 // import WelcomePageComp from "./components/WelcomePage/WelcomePage"
-import MenuPageComp from "./components/MenuPage/MenuPage";
+// import MenuPageComp from "./components/MenuPage/MenuPage";
+import ImportMenuComp from "./components/ImportMenu/ImportMenu";
+// import AddNewOrderComp from "./components/AddNewOrder/AddNewOrder";
+// import AddNewTableComp from "./components/AddNewTable/AddNewTable";
 
 
 
@@ -24,8 +27,9 @@ function App() {
       {/* <Passport /> */}
       <NavbarComp />
       {/* <WelcomePageComp /> */}
-      <MenuPageComp />
+      {/* <MenuPageComp /> */}
       {/* <LoginComp /> */}
+      <ImportMenuComp/>
       {/* <SignupComp /> */}
       {/* <AddNewTableComp /> */}
       {/* <AddNewOrderComp /> */}
