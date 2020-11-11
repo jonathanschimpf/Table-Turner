92d44b4153592e0ca84e2a0a789c2c7595a79c15
James Lamb <jameslamb13@gmail.com>
2020-11-11 13:58:49 -0500

    tables are created dynamically with correct associated orders in modal.

--- client/src/components/ViewAllTables/ViewAllTables.js
+++ client/src/components/ViewAllTables/ViewAllTables.js
@@ -19,42 +19,50 @@ function ViewAllTablesComp() {
             .catch(err => console.log(err));
     };
 
-    // --- below will be used later --- //
-    // item.label, item.item, item.extra_notes, item.allergies, item.course
-
-      // for(var i = 0; i < items.length; i++){
     
-    // var tables = items.filter(item => {
-    //     return item.table === i
-    // }).map(item => {
-    //     return [item.table];
-    // });
-    // console.log(tables)
-    
-    
-    // } 
 
     const [items, setItems] = useState([]);
+
+    //FILTER MAP METHOD
+    const filteredOrders = function (tableNumb) {
+       
     
-    var allTables = items.reduce((tables, item) => {
-        if(tables.includes(item.table)){
-            allTables.push(item.table)
+            var tables = items.filter(item => {
+                return item.table === tableNumb
+            }).map(item => {
+                return {table: item.table, label: item.label, order: item.order, notes: item.extra_notes, allergies: item.allergies, course: item.course};
+            });
+            console.log(tables, "filtered")
+            return(tables)
+            
+            
+    }
+
+    
+    
+    //REDUCE METHOD
+
+    const allTables = items.map(item => {
+            return [item.table];
+        });
+    console.log(allTables, "allTables")
+        
+
+    const reducedTables = allTables.reduce((tableNumbs, table) => {
+        
+        if(!tableNumbs.includes(table[0])){
+            
+            tableNumbs.push(table[0])
+            
         }
-        return allTables
+        
+        return tableNumbs
+
     }, [])
-    console.log(allTables)
+    console.log(reducedTables, "reducedTables")
 
-  
-
-    // Not being used yet // 
     const [modalTable, setModalTable] = useState([]);
-    const [modalLabel, setModalLabel] = useState([]);
-    const [modalItem, setModalItem] = useState([]);
-    const [modalNotes, setModalNotes] = useState([]);
-    const [modalAllergies, setModalAllergies] = useState([]);
-    const [modalCourse, setModalCourse] = useState([]);
-    // Not being used yet //
-
+    
 
     const [modalShow, setModalShow] = React.useState(false);
 
@@ -69,49 +77,52 @@ function ViewAllTablesComp() {
                 <br />
                 <br />
                 
-                {/* {allTables.map(item =>    */}
+                {reducedTables.map(tableNumb =>   
                 
                 <Jumbotron className="jumbotronTableView">
-                    <h2 className="text-center responsiveH2Font">Table</h2>
+                    <h2 className="text-center responsiveH2Font">Table {tableNumb}</h2>
                     <hr style={{ height: '4px' }} />
 
                     <Form>
 
                         <Row>
                             <Col xs={9}>
-                                <Button variant="outline-dark" className="my-2 my-lg-0 formControl view tableViewButtons" size="sm" variant="dark" onClick={() => setModalShow(true)} block>View Details</Button>
+                                <Button variant="outline-dark" className="my-2 my-lg-0 formControl view tableViewButtons" size="sm" variant="dark" onClick={() => {
+                                 setModalTable(tableNumb)
+                                 setModalShow(true)
+                                 
+                                 }} block>View Details</Button>
 
-                                <MyVerticallyCenteredModal
-                                    show={modalShow}
-                                    onHide={() => setModalShow(false)} />
+                                
                             </Col>
 
                             <Col xs={3}>
                                 <Button variant="outline-danger" className="my-2 my-lg-0 formControl view deleteTableButtons" 
                                 size="sm" block onClick={() => {
-                                    setModalTable(item[0])
-                                    setModalLabel(item[1])
-                                    setModalItem(item[2])
-                                    setModalNotes(item[3])
-                                    setModalAllergies(item[4])
-                                    setModalCourse(item[5])
                                     
-                                
                                     setSmallerModalShow(true)
                                 }} >
                                 
                                 <strong>x</strong></Button>
 
-                                <MySmallerVerticallyCenteredModal
-                                    show={smallermodalShow}
-                                    onHide={() => setSmallerModalShow(false)} />
+                                
                             </Col>
                         </Row>
 
                     </Form>
 
                 </Jumbotron>
-            {/* )} */}
+              
+             )}
+
+            <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)} />
+
+            <MySmallerVerticallyCenteredModal
+            show={smallermodalShow}
+            onHide={() => setSmallerModalShow(false)} />
+            
             </Container>
 
             <br />
@@ -127,6 +138,9 @@ function ViewAllTablesComp() {
 
     // this function is for the table order details modal
     function MyVerticallyCenteredModal(props) {
+
+
+
         return (
             <Modal
                 {...props}
@@ -143,6 +157,7 @@ function ViewAllTablesComp() {
                     <p className="card-text"><small className="text-muted"><strong>Order Details</strong></small></p>
                     <Table responsive className="tableBlackGround" size="sm" striped bordered hover variant="dark">
                         <thead>
+                            
                             <tr>
                                 <th>Item</th>
                                 <th>Allergy Category</th>
@@ -151,32 +166,23 @@ function ViewAllTablesComp() {
                                 <th>Course#</th>
                                 <th className="deleteFont">Delete</th>
                             </tr>
+                            
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Bibimbap</td>
-                                <td>Egg</td>
-                                <td>Hold the Eggs + Sesame Seeds.</td>
-                                <td>Bob</td>
-                                <td className="centeredCourseNumber">1</td>
+                        {reducedTables.map(item => { 
+                        const table = filteredOrders(item); 
+                        return table.map(order  => { if (order.table === modalTable) 
+                        return  <tr>
+                                <td>{order.order}</td>
+                                <td>{}</td>
+                                <td>{order.notes}</td>
+                                <td>{order.label}</td>
+                        <td className="centeredCourseNumber">{order.course}</td>
                                 <td className="removeRow"><strong>x</strong></td>
-                            </tr>
-                            <tr>
-                                <td>Bulgogi</td>
-                                <td>N/A</td>
-                                <td>Make Extra spicy!</td>
-                                <td>Met's Hat Guy</td>
-                                <td className="centeredCourseNumber">1</td>
-                                <td className="removeRow"><strong>x</strong></td>
-                            </tr>
-                            <tr>
-                                <td>Kimchi-Jjigae</td>
-                                <td>Shellfish</td>
-                                <td>No Seafood or Fish Sauce.</td>
-                                <td>Seat 3</td>
-                                <td className="centeredCourseNumber">2</td>
-                                <td className="removeRow"><strong>x</strong></td>
-                            </tr>
+                             </tr>
+                        })}
+                             )} 
+                            
                         </tbody>
                     </Table>
     
